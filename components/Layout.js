6e75ec00d7ed6239e2c460fb0d6e6673e9a67423
satyam mishra <satyam.mishra975@gmail.com>
2022-03-16 14:09:47 +0530

    make changes to improve lighthouse score's

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,16 @@
 import TopBar from './TopBar'
 import Footer from './Footer'
+import Head from 'next/head'
 export default function Layout({ children }) {
     const {props} = children
     const {data} = props
   return (
     <>
+      <Head>
+        <html lang='en'></html>
+        <title>Marvel</title>
+        <meta name="description" content="Display 20 marvel superhero's"></meta>
+      </Head>
       <TopBar />
       <main className='mx-4 mt-4 mb-10'>{children}</main>
       <Footer/>
