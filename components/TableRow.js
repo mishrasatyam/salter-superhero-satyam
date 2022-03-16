cd7141a2506a4fe1d9eae326a34feab7cb721951
satyam mishra <satyam.mishra975@gmail.com>
2022-03-16 13:47:09 +0530

    display not found if character not found

--- components/TableRow.js
+++ components/TableRow.js
@@ -17,6 +17,8 @@ const TableRow = ({data}) => {
     }
     return (
     <div className="h-full shadow-lg rounded-lg p-2">
+        {data.code==200?
+        <>
         <Image alt="Vercel logo" src={data.data.thumbnail.path+'.'+data.data.thumbnail.extension} layout="responsive" width={1000} height={1000} />
         <div className="">
             <h3 className="text-center p-2 text-lg text-primary font-bold hover:underline cursor-pointer" onClick={()=>handleClick(data)}>{data.data.name}</h3>
@@ -25,6 +27,12 @@ const TableRow = ({data}) => {
             <h3 className="text-center mb-5">{data.data.stories.available} <span className="text-xs text-primary font-bold">stories</span></h3>
             <h3 className="leading-6 tracking-widest">{data.data.description || 'Description not available'}</h3>
         </div>
+        </>
+        :
+        <div className="text-center mt-20">
+                <span className="font-bold">Not found!</span>
+        </div>
+        }
         {show_popup?<Popup data={popup_data} closePopup = {closePopup}/>:''}
     </div>
     )
